834d2302e9d35c42b394015b6770a9063ad81f81
alubame001 <alubame001@gmail.com>
2018-10-11 16:22:13 +0800

    upd

--- contracts/EOSPixels/EOSPixels.cpp
+++ contracts/EOSPixels/EOSPixels.cpp
@@ -203,7 +203,7 @@ void eospixels::onTransfer(const currency::transfer &transfer) {
 
   action(permission_level{_self, N(active)}, N(eosio.token), N(transfer),
          std::make_tuple(_self, transfer.from, quantity,
-                         std::string(tx_hash)))
+                         std::string(s))
       .send();
 
    auto player = *accountItr;
